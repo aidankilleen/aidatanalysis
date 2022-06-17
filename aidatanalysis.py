34ae533bc6c8c32708b2f433d2c5a67808f81b17
Aidan Killeen <aidan.killeen@gmail.com>
2022-06-17 08:45:32 +0100

    added setup.py

--- aidatanalysis.py
+++ aidatanalysis.py
@@ -1,3 +1,5 @@
+__version__ = "0.0.1"
+
 def analyse_data(list):
     sum = 0
     count = 0
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from aidatanalysis import __version__
+
+setup(
+    name='aidatanalysis',
+    version=__version__,
+
+    url='https://github.com/aidankilleen/aidatanalysis',
+    author='Aidan Killeen',
+    author_email='aidan.killeen@gmail.com',
+
+    py_modules=['aidatanalysis'],
+)
